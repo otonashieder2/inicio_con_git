5a4cd73ada1556a1504f23c7e8880e4d8ee95c63
Eder <bmw_egc1986@hotmail.com>
2020-12-26 15:52:45 -0500

    inicio con git

---
+++ pruebas.py
@@ -0,0 +1,26 @@
+#for i in ["primavera","verano","otogno","invierno"]:
+ #   print(i,end=" ")
+
+#email=False
+#miEmail=input("introduce tu correo electronico:  ")
+#for i in miEmail:
+ #   if (i=="@" ) :
+  #     email=True
+#if email==True :
+ # print("el email es correcto")
+#else :
+ # print("el email es incorrecto")  
+#print("el programa termino")  
+
+
+
+#alido=False
+email=input("introduce tu email :  ")
+for i in range(len(email)):
+    if email[i]=="@" :
+        valido=True
+if valido==True:
+    print("email correcto")
+else :
+ print("email incorrecto")            
+
